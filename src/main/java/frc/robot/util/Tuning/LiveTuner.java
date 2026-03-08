0210b59e2b0f6880d5d8d7e70f19356c773e3715
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-03-07 17:56:52 -0800

    create a new live tuner set for PIDSVA and magic motion configs.

--- src/main/java/frc/robot/util/Tuning/LiveTuner.java
+++ src/main/java/frc/robot/util/Tuning/LiveTuner.java
@@ -52,6 +52,46 @@ public final class LiveTuner {
     kD.onChange(v -> apply.run());
   }
 
+    // Create a tunable PID set
+  public static void pidMagicMotion (
+      String name,
+      double defaultP,
+      double defaultI,
+      double defaultD,
+      double defaultS,
+      double defaultV,
+      double defaultA,
+      double defaultCruiseVel,
+      double defaultAccel,
+      double defaultJerk,
+      PIDMagicMotionConsumer consumer
+  ) {
+    TunableNumber kP = number(name + "/kP", defaultP);
+    TunableNumber kI = number(name + "/kI", defaultI);
+    TunableNumber kD = number(name + "/kD", defaultD);
+    TunableNumber kS = number(name + "/kS", defaultS);
+    TunableNumber kV = number(name + "/kV", defaultV);
+    TunableNumber kA = number(name + "/kA", defaultA);
+    TunableNumber cruiseVel = number(name + "/MotionMagicCruiseVel", defaultCruiseVel);
+    TunableNumber accel = number(name + "/MotionMagicAccel", defaultAccel);
+    TunableNumber jerk = number(name + "/MotionMagicJerk", defaultJerk);
+
+    Runnable apply = () -> consumer.accept(
+    kP.get(), kI.get(), kD.get(), 
+    kS.get(), kV.get(), kA.get(), 
+    cruiseVel.get(), accel.get(), jerk.get());
+
+    kP.onChange(v -> apply.run());
+    kI.onChange(v -> apply.run());
+    kD.onChange(v -> apply.run());
+    kS.onChange(v -> apply.run());
+    kV.onChange(v -> apply.run());
+    kA.onChange(v -> apply.run());
+    cruiseVel.onChange(v -> apply.run());
+    accel.onChange(v -> apply.run());
+    jerk.onChange(v -> apply.run());
+  }
+
   private TunableNumber createNumber(String key, double defaultValue) {
     int id = idGenerator.incrementAndGet(); // kept only for map keys
     TunableNumber number = new TunableNumber(key, defaultValue);
@@ -128,5 +168,13 @@ public final class LiveTuner {
   @FunctionalInterface
   public interface PIDConsumer {
     void accept(double kP, double kI, double kD);
+  }
+
+  public interface PIDMagicMotionConsumer {
+    void accept(
+      double kP, double kI, double kD,
+      double kS, double kV, double kA,
+      double cruiseVel, double accel, double jerk
+    );
   }
 }
