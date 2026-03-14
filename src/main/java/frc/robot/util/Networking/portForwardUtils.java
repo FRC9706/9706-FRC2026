895bb30f510c724e376d392cb7d422f5dc96c0cf
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-03-14 12:44:21 -0700

    reform and reorganization

--- src/main/java/frc/robot/util/Networking/portForwardUtils.java
+++ src/main/java/frc/robot/util/Networking/portForwardUtils.java
@@ -1,7 +1,5 @@
 package frc.robot.util.Networking;
 import edu.wpi.first.net.PortForwarder;
-import frc.robot.subsystems.Vision.VisionConstants;
-import frc.robot.util.Tuning.LiveTuner;
 
 
 public class portForwardUtils {
@@ -30,56 +28,5 @@ public class portForwardUtils {
             int remotePort = remoteStartPort + i;
             PortForwarder.remove(remotePort);
         }
-    }
-
-
-    public static void portFowardLL(String LL) {
-        if (LL == "FR") {
-        forwardPortRange(VisionConstants.limelightFrontRightIP, 
-         5800, 5805, 5800, 5805);
-        } else if (LL == "FL") {
-        forwardPortRange(VisionConstants.limelightFrontLeftIP, 
-         5800, 5805, 5800, 5805); 
-        }
-
-        if (LL == "BR") {
-        forwardPortRange(VisionConstants.limelightBackRightIP, 
-         5800, 5805, 5800, 5805); 
-        } else if (LL == "BL") {
-        forwardPortRange(VisionConstants.limelightBackLeftIP, 
-         5800, 5805, 5800, 5805); 
-        }
-    }
-
-    public static void removeFowardedLL() {
-        removeForwardedPorts(5800, 5805); 
-    }
-
-    public static void createPortFowardSlider() {
-            LiveTuner.choice(
-            "PortForwarder/LL", 
-            0, 
-            new String[]{"FrontRight", "FrontLeft", "BackRight", "BackLeft"}, 
-            LLindex -> {
-                switch(LLindex) {
-                    case 0:
-                        removeFowardedLL();
-                        portFowardLL("FR");
-                        break;
-                    case 1:
-                        removeFowardedLL();
-                        portFowardLL("FL");
-                        break;
-                    case 2:
-                        removeFowardedLL();
-                        portFowardLL("BR");
-                        break;
-                    case 3:
-                        removeFowardedLL();
-                        portFowardLL("BL");
-                        break;
-                }
-            }
-        );
     }
 }
