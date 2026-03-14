fdce99dd4305d1cf3fa816895572ce269621984d
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-03-14 10:36:13 -0700

    complete reform, support dynamic selection

--- src/main/java/frc/robot/util/Vision/portForwardUtils.java
+++ src/main/java/frc/robot/util/Vision/portForwardUtils.java
@@ -1,10 +1,11 @@
 package frc.robot.util.Vision;
 import edu.wpi.first.net.PortForwarder;
 import frc.robot.subsystems.Vision.VisionConstants;
+import frc.robot.util.Tuning.LiveTuner;
 
 
 public class portForwardUtils {
-    portForwardUtils(){} // This is a utility class; don't load constructor
+    portForwardUtils() {} // Static utility; don't load constructor
     /**
      * Forwards all ports in [startPort, endPort] from the roboRIO
      * to the given remote host using the same remote port number.
@@ -14,8 +15,7 @@ public class portForwardUtils {
         int localStartPort, 
         int localEndPort,
         int remoteStartPort, 
-        int remoteEndPort
-    ) {
+        int remoteEndPort) {
         for (int i = 0; i <= localEndPort - localStartPort; i++) {
             int localPort = localStartPort + i;
             int remotePort = remoteStartPort + i;
@@ -23,12 +23,63 @@ public class portForwardUtils {
         }
     }
 
+    public static void removeForwardedPorts(
+        int remoteStartPort, 
+        int remoteEndPort) {
+        for (int i = 0; i <= remoteEndPort - remoteStartPort; i++) {
+            int remotePort = remoteStartPort + i;
+            PortForwarder.remove(remotePort);
+        }
+    }
 
-    public static void portFoward() {
-        forwardPortRange(VisionConstants.limelightTurretIP, 
+
+    public static void portFowardLL(String LL) {
+        if (LL == "FR") {
+        forwardPortRange(VisionConstants.limelightFrontRightIP, 
          5800, 5805, 5800, 5805);
-    
-        // forwardPortRange(Constants.limelights.limelightFrontIP, 
-        //    5800, 5809, 5800, 5809);
+        } else if (LL == "FL") {
+        forwardPortRange(VisionConstants.limelightFrontLeftIP, 
+         5800, 5805, 5800, 5805); 
+        }
+
+        if (LL == "BR") {
+        forwardPortRange(VisionConstants.limelightBackRightIP, 
+         5800, 5805, 5800, 5805); 
+        } else if (LL == "BL") {
+        forwardPortRange(VisionConstants.limelightBackLeftIP, 
+         5800, 5805, 5800, 5805); 
+        }
+    }
+
+    public static void removeFowardedLL() {
+        removeForwardedPorts(5800, 5805); 
+    }
+
+    public static void createPortFowardSlider() {
+            LiveTuner.choice(
+            "PortForwarder/LL", 
+            0, 
+            new String[]{"FrontRight", "FrontLeft", "BackRight", "BackLeft"}, 
+            LLindex -> {
+                switch(LLindex) {
+                    case 0:
+                        removeFowardedLL();
+                        portFowardLL("FR");
+                        break;
+                    case 1:
+                        removeFowardedLL();
+                        portFowardLL("FL");
+                        break;
+                    case 2:
+                        removeFowardedLL();
+                        portFowardLL("BR");
+                        break;
+                    case 3:
+                        removeFowardedLL();
+                        portFowardLL("BL");
+                        break;
+                }
+            }
+        );
     }
 }
