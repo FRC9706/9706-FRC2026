4fc27b7c2c0fd49f34a83fc3ce129f5f5c85c768
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-03-19 16:24:04 -0700

    Actually begin using preloader; added a command registering function + overhualed comments

--- src/main/java/frc/robot/util/Pathplanner/Preloader.java
+++ src/main/java/frc/robot/util/Pathplanner/Preloader.java
@@ -1,5 +1,6 @@
 package frc.robot.util.Pathplanner;
 
+import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.wpilibj2.command.Command; // Import the Command class
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -11,23 +12,55 @@ public class Preloader {
     /**
      * A static container class to hold all of the preloaded PathPlanner Commands.
      */
-    public static class preloadedPaths {
+    public static class preloadedAutos {
         // Declare the fields as static so they can be initialized in the static preload method.
         // The type should be PathPlannerAuto, which is a Command.
-        public static Command pathOne; 
-        public static Command pathTwo;
+        public static Command rigthAuto; 
+        public static Command middleAuto;
+        public static Command leftAuto;
     }
 
-    // Preloads all of the desired trajctories
-    public static void preloadPaths() {
+    /**
+     * Preloads autos; preventing delays when automous is activated.
+     * @implNote Assure that your autos are configured in the {@code preloadedAutos} class & in the function
+     * @implNote PathPlannerAuto will load the path file with the given name (e.g., "autohOne.auto")
+     * @implNote Make sure named commands are setup for said path.
+     */
+    public static void preloadsAutos() {
         // Assign the new PathPlannerAuto commands to the static fields.
-        // NOTE: PathPlannerAuto will load the path file with the given name (e.g., "pathOne.path").
-        preloadedPaths.pathOne = new PathPlannerAuto("pathOne");
-        preloadedPaths.pathTwo = new PathPlannerAuto("pathTwo");
+        // NOTE: PathPlannerAuto will load the path file with the given name (e.g., "autohOne.auto")
+        // It will also automatically load ALL referenced .path files inside the auto.
+        preloadedAutos.rigthAuto = new PathPlannerAuto("rightAuto");
+        preloadedAutos.middleAuto = new PathPlannerAuto("middleAuto");
+        preloadedAutos.leftAuto = new PathPlannerAuto("leftAuto");
         
     
         System.out.println("PathPlanner paths have been preloaded!");
     }
+    /**
+     * Reigsters named commands for any given auto
+     * @param auto The auto whose named commands to register.
+     * @implNote Assure that the auto you are passing in is handled in the function
+     */
+    public static void registerNamedCmds(String auto) {
+        switch(auto) {
+            case "rightAuto":
+                NamedCommands.clearAll();
+                NamedCommands.registerCommand("ben", null);
+            break;
+
+            case "centerAuto":
+                NamedCommands.clearAll();
+                NamedCommands.registerCommand("is", null);
+            break;
+
+            case "leftAuto": 
+                NamedCommands.clearAll();
+                NamedCommands.registerCommand("ben", null);
+            break;
+        }
+    }
+    
 
     /**
      * Utility method to retrieve a preloaded command.
@@ -36,8 +69,9 @@ public class Preloader {
      */
     public static Command getpath(String pathName) {
         return switch (pathName) {
-            case "pathOne" -> preloadedPaths.pathOne;
-            case "pathTwo" -> preloadedPaths.pathTwo;
+            case "rightAuto" -> preloadedAutos.rigthAuto;
+            case "middleAuto" -> preloadedAutos.middleAuto;
+            case "leftAuto" -> preloadedAutos.leftAuto;
             default -> {
                 // Handle case where path is not found (e.g., return an empty command or throw error)
                 System.err.println("Requested PathPlanner path not preloaded: " + pathName);
