6c0a5ae5364d46d32d4a842a3d2b7d71edf7b202
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-03-19 16:22:54 -0700

    Create and setup the indexer subsystem

---
+++ src/main/java/frc/robot/subsystems/Indexer/Spindexer.java
@@ -0,0 +1,57 @@
+package frc.robot.subsystems.Indexer;
+
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.Networking.DynamicInputs;
+
+public class Spindexer extends SubsystemBase{
+    // Singleton instance
+    private static Spindexer mInstance = null;
+    public static synchronized Spindexer getInstance() {
+        if (mInstance == null) {
+            mInstance = new Spindexer();
+        }
+        return mInstance;
+    }
+
+    // Hardware
+    TalonFX motor;
+
+    public Spindexer() {
+        // Intialize Motors
+        motor = new TalonFX(SpindexerConstants.motorID);
+
+        // Configure motors
+        MotorConfigs.configureMotors(motor);
+
+        // Setup live tuning for spindexer PID
+        DynamicInputs.pid(
+            "Spindexer/PID", 
+            SpindexerConstants.kPID[0], 
+            SpindexerConstants.kPID[1], 
+            SpindexerConstants.kPID[2],
+            (p, i, d) -> MotorConfigs.configureSpinPID(motor, p, i, d)
+        );
+    }
+
+    public void stopDexer() {
+        motor.stopMotor();
+    }
+    
+    public void spinDexer(double rpm) {
+        // rpm -> velocity
+        double vel = rpm/60;
+        System.out.println("Spinning at RPM: " + rpm + " Velocity: " + vel);
+
+        // velocity control
+        final VelocityVoltage m_request = new VelocityVoltage(vel).withSlot(0);
+        motor.setControl(m_request);
+    }
+
+    @Override
+    public void periodic() {
+        // Nothing here yet
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Indexer/SpindexerConstants.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.Indexer;
+
+public class SpindexerConstants {
+    // Motor IDs
+    public static int motorID = 1;
+
+    // Motor variables
+    public static int maxVoltage = 10;
+
+    public static double[] kPID = {
+        0, // kP
+        0, // kI
+        0 // kD
+    };
+
+
+}
