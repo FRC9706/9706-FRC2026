ef8f1f3a0262d4df4fa59f552ea503ed677c34b1
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-03-21 12:05:02 -0700

    adapt for dynamicTuner rebrand; use getLimelightInst()

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj.Timer;
 // import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.util.Tuning.LiveTuner;
+import frc.robot.util.Networking.DynamicInputs;
 import swervelib.simulation.ironmaple.simulation.SimulatedArena;
 
 /**
@@ -55,10 +55,10 @@ public class Robot extends LoggedRobot {
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
     // Live tuner updates
-    LiveTuner.periodic();
+    DynamicInputs.periodic();
 
     // Use limelight vision measurements to estimate robot position
-    m_robotContainer.limelightInstance.createVisionMeasurements(m_robotContainer, Timer.getTimestamp());
+    m_robotContainer.getLimelightInst().createVisionMeasurements(m_robotContainer, Timer.getTimestamp());
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
