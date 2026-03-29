735bf1e1d4e617e5362c96f0eecbc0c127edb3e2
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-03-28 18:15:34 -0700

    comment controls

--- src/main/java/frc/robot/util/Controller/ControllerConfigurator.java
+++ src/main/java/frc/robot/util/Controller/ControllerConfigurator.java
@@ -6,8 +6,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotContainer;
-import frc.robot.commands.Hopper.HopperControl;
-import frc.robot.subsystems.Hopper.Hopper.hopperState;
 import lib.Networking.DynamicInputs;
 import lib.Networking.DynamicInputs.dynamicNum;
 
@@ -34,13 +32,13 @@ public class ControllerConfigurator {
         //     Commands.run(() -> container.getDrivebase().drive(new ChassisSpeeds(0.3, 0, 0)))
         // );
 
-        container.getMDriverController().y().whileTrue(
-            Commands.run(() -> container.getTurretBetaInst().resetRotEncoderPositons())
-        );
+        // container.getMDriverController().y().whileTrue(
+        //     Commands.run(() -> container.getTurretBetaInst().resetRotEncoderPositons())
+        // );
 
-        container.getMDriverController().x().whileTrue(
-            Commands.run(() -> container.getTurretBetaInst().smartMoveToHub())
-        );
+        // container.getMDriverController().x().whileTrue(
+        //     Commands.run(() -> container.getTurretBetaInst().smartMoveToHub())
+        // );
 
         // Reset Gyro/Oreint robot to current facing position
         container.getMDriverController().a().onTrue(
@@ -50,35 +48,19 @@ public class ControllerConfigurator {
             )
         );
 
-        container.getMDriverController().b().whileTrue(
-            Commands.run(() -> container.getTurretBetaInst().smartMoveTurretToPos(-190.0/360.0))
-        );
+        // container.getMDriverController().b().whileTrue(
+        //     Commands.run(() -> container.getTurretBetaInst().smartMoveTurretToPos(-190.0/360.0))
+        // );
 
-        motorRPM = DynamicInputs.number("Turret/ShootRPM", 3400);
+        // motorRPM = DynamicInputs.number("Turret/ShootRPM", 3400);
 
-        container.getMDriverController().leftBumper().onTrue(
-            Commands.runOnce(() -> container.getTurretBetaInst().shoot(motorRPM.get())))
-            .onFalse(Commands.runOnce(() -> container.getTurretBetaInst().stopShootMotors()));
-
-        container.getMDriverController().povUp().onTrue(
-            new HopperControl(container.getHopperInst(), hopperState.EXTENDED)
-        );
-
-        container.getMDriverController().povDown().onTrue(
-            new HopperControl(container.getHopperInst(), hopperState.RETRACTED)
-        );
-
-        container.getMDriverController().povRight().onTrue(
-            new HopperControl(container.getHopperInst(), hopperState.WIGGLE)
-        );
-
-        container.getMDriverController().povLeft().onTrue(
-            new HopperControl(container.getHopperInst(), hopperState.IDLE)
-        );
+        // container.getMDriverController().leftBumper().onTrue(
+        //     Commands.runOnce(() -> container.getTurretBetaInst().shoot(motorRPM.get())))
+        //     .onFalse(Commands.runOnce(() -> container.getTurretBetaInst().stopShootMotors()));
 
    }
 
-    public static void configureControllerSim(RobotContainer container) {
+    public void configureControllerSim(RobotContainer container) {
 
       container.getMDriverController().start().onTrue(Commands.runOnce(() -> 
         container.getDrivebase().resetOdometry(
@@ -86,21 +68,21 @@ public class ControllerConfigurator {
         )
       ));
       
-        container.getMDriverController().povUp().onTrue(
-            new HopperControl(container.getHopperInst(), hopperState.EXTENDED)
-        );
+        // container.getMDriverController().povUp().onTrue(
+        //     new HopperControl(container.getHopperInst(), hopperState.EXTENDED)
+        // );
 
-        container.getMDriverController().povDown().onTrue(
-            new HopperControl(container.getHopperInst(), hopperState.RETRACTED)
-        );
+        // container.getMDriverController().povDown().onTrue(
+        //     new HopperControl(container.getHopperInst(), hopperState.RETRACTED)
+        // );
 
-        container.getMDriverController().povRight().onTrue(
-            new HopperControl(container.getHopperInst(), hopperState.WIGGLE)
-        );
+        // container.getMDriverController().povRight().onTrue(
+        //     new HopperControl(container.getHopperInst(), hopperState.WIGGLE)
+        // );
 
-        container.getMDriverController().povLeft().onTrue(
-            new HopperControl(container.getHopperInst(), hopperState.IDLE)
-        );
+        // container.getMDriverController().povLeft().onTrue(
+        //     new HopperControl(container.getHopperInst(), hopperState.IDLE)
+        // );
     }
 
     public static void configureControllerTest(RobotContainer container) {}
