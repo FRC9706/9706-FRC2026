985b4b851df87dfed1b48929c040585953ec0bc0
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-03-30 19:45:52 -0700

    added hopper and changed desgin again?

--- src/main/java/frc/robot/subsystems/Hopper/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper/Hopper.java
@@ -1,112 +1,132 @@
-// package frc.robot.subsystems.Hopper;
+package frc.robot.subsystems.Hopper;
 
-// import com.ctre.phoenix6.controls.MotionMagicVoltage;
-// import com.ctre.phoenix6.hardware.TalonFX;
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import lib.Networking.DynamicInputs;
-// import lombok.Getter;
+import org.littletonrobotics.junction.Logger;
 
-// public class Hopper extends SubsystemBase {
-//     // Singleton instance
-//     private static Hopper mInstance = null;
-//     public static synchronized Hopper getInstance() {
-//         if (mInstance == null) {
-//             mInstance = new Hopper();
-//         }
-//         return mInstance;
-//     }
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lib.Networking.DynamicInputs;
+import lombok.Getter;
 
-//     // State machine
-//     @Getter private hopperState currHopperState = hopperState.IDLE;
+public class Hopper extends SubsystemBase {
+    // Singleton instance
+    private static Hopper mInstance = null;
+    public static synchronized Hopper getInstance() {
+        if (mInstance == null) {
+            mInstance = new Hopper();
+        }
+        return mInstance;
+    }
 
-//     public enum hopperState {
-//         IDLE,
-//         EXTENDED,
-//         RETRACTED,
-//         WIGGLE
-//     }
+    // State machine
+    @Getter private hopperState currHopperState = hopperState.IDLE;
 
-//     public void setHopperState(hopperState state) {
-//         currHopperState = state;
-//     } 
+    public enum hopperState {
+        IDLE,
+        EXTENDED,
+        RETRACTED,
+        WIGGLE
+    }
 
-//     public void findState() {
-//         if (hasReachedPos(HopperConstants.retractedPos)) {
-//             setHopperState(hopperState.RETRACTED);
-//         } else if (hasReachedPos(HopperConstants.extendedPos)) {
-//             setHopperState(hopperState.EXTENDED);
-//         } else {
-//             setHopperState(hopperState.IDLE);
-//         }
-//     }
+    public void setHopperState(hopperState state) {
+        currHopperState = state;
+    } 
+
+    public void findState() {
+        if (hasReachedPos(HopperConstants.retractedPos)) {
+            setHopperState(hopperState.RETRACTED);
+        } else if (hasReachedPos(HopperConstants.extendedPos)) {
+            setHopperState(hopperState.EXTENDED);
+        } else {
+            setHopperState(hopperState.IDLE);
+        }
+    }
     
 
-//     // initalize hardware variables
-//     TalonFX[] hopperMotors;
+    // initalize hardware variables
+    @Getter private TalonFX[] hopperMotors;
 
-//     // initalize motion profile variable
-//     private MotionMagicVoltage m_request;
+    // initalize motion profile variable
+    private MotionMagicVoltage m_request;
 
-//     public Hopper() {
-//         // intialize hopper motors
-//         hopperMotors = new TalonFX[] {
-//             new TalonFX(HopperConstants.hopperMotors[0]),
-//             new TalonFX(HopperConstants.hopperMotors[1])
-//         };
+    public Hopper() {
+        // intialize hopper motors
+        hopperMotors = new TalonFX[] {
+            new TalonFX(HopperConstants.hopperMotors[0]),
+            new TalonFX(HopperConstants.hopperMotors[1])
+        };
 
-//         // Configure hopper motors
-//         MotorConfigs.configureMotors(hopperMotors);
+        // Configure hopper motors and reset positions
+        MotorConfigs.configureMotors(hopperMotors);
+        for (TalonFX motor : hopperMotors) {
+            motor.setPosition(0);
+        }
 
-//         // setup live tuner for the hopper motors
-//         DynamicInputs.pidMagicMotion("Hopper/HopperPID", 
-//             HopperConstants.kPID[0], 
-//             HopperConstants.kPID[1], 
-//             HopperConstants.kPID[2], 
-//             HopperConstants.kPID[3],
-//             HopperConstants.kPID[4],
-//             HopperConstants.kPID[5],
-//             HopperConstants.kPID[6],
-//             HopperConstants.kPID[7],
-//             HopperConstants.kPID[8],
-//             (p, i, d, 
-//                 s, y, b, a, u, 
-//             ok) -> 
-//                 MotorConfigs.configurePID(
-//                     hopperMotors, p, i, d, 
-//                         s, y, b, a, u, 
-//                     ok)
-//         );
-//     }
+        // setup live tuner for the hopper motors
+        DynamicInputs.pidMagicMotion("Hopper/HopperPID", 
+            HopperConstants.kPID[0], 
+            HopperConstants.kPID[1], 
+            HopperConstants.kPID[2], 
+            HopperConstants.kPID[3],
+            HopperConstants.kPID[4],
+            HopperConstants.kPID[5],
+            HopperConstants.kPID[6],
+            HopperConstants.kPID[7],
+            HopperConstants.kPID[8],
+            (p, i, d, 
+                s, y, b, a, u, 
+            ok) -> 
+                MotorConfigs.configurePID(
+                    hopperMotors, p, i, d, 
+                        s, y, b, a, u, 
+                    ok)
+        );
+    }
 
-//     public double getHopperPos() {
-//         return hopperMotors[0].getPosition().getValueAsDouble();
-//     }
+    public double getHopperPos() {
+        return hopperMotors[0].getPosition().getValueAsDouble();
+    }
 
-//     public void stopHopperMotors() {
-//         hopperMotors[0].stopMotor();
-//     }
+    public double getHopperPos2() {
+        return hopperMotors[1].getPosition().getValueAsDouble();
+    }
 
-//     public void moveHopperToPos(double pos) {
-//         m_request = new MotionMagicVoltage(0).withPosition(pos);
-//         hopperMotors[0].setControl(m_request);
-//     }
+    public void stopHopperMotors() {
+        hopperMotors[0].stopMotor();
+        hopperMotors[1].stopMotor();
+    }
 
-//     public boolean hasReachedPos(double pos) {
-//         boolean hasReachedPos = (Math.abs(getHopperPos() - pos) <= HopperConstants.posTolerance);
-//         return hasReachedPos;
-//     }
+    public void moveHopperToPos(double pos) {
+        m_request = new MotionMagicVoltage(0).withPosition(pos);
+        hopperMotors[0].setControl(m_request);
+        hopperMotors[1].setControl(m_request);
+    }
 
-//     public void retractMotors() {
-//         moveHopperToPos(HopperConstants.retractedPos);
-//     }
+    public boolean hasReachedPos(double pos) {
+        boolean hasReachedPos = (Math.abs(getHopperPos() - pos) <= HopperConstants.posTolerance);
+        boolean hasReachedPos2 = (Math.abs(getHopperPos2() - pos) <= HopperConstants.posTolerance);
+        if (hasReachedPos && hasReachedPos2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-//     public void extendMotors() {
-//         moveHopperToPos(HopperConstants.extendedPos);
-//     }
+    public void retractMotors() {
+        moveHopperToPos(HopperConstants.retractedPos);
+    }
 
-//     @Override
-//     public void periodic() {
-//         // find the motor's state and set them to their respective state
-//         findState();
-//     }
-// }
+    public void extendMotors() {
+        moveHopperToPos(HopperConstants.extendedPos);
+    }
+
+    @Override
+    public void periodic() {
+        // find the motor's state and set them to their respective state
+        findState();
+
+        // Log some values
+        Logger.recordOutput("Hopper/Motors/Right Motor Position", getHopperPos());
+        Logger.recordOutput("Hopper/Motors/Left Motor Position", getHopperPos2());
+    }
+}
