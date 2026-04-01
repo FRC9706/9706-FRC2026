04cc62318a933c2be02e99c18e67417eb6d56fe8
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-03-31 18:21:36 -0700

    rename & getter

--- src/main/java/frc/robot/subsystems/Indexer/Spindexer.java
+++ src/main/java/frc/robot/subsystems/Indexer/Spindexer.java
@@ -5,18 +5,19 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lombok.Getter;
 public class Spindexer extends SubsystemBase{
     // Hardware
-    TalonFX motor;
+    @Getter TalonFX spindexerMotor;
 
     private DutyCycleOut request = new DutyCycleOut(0);
 
     public Spindexer() {
         // Intialize Motors
-        motor = new TalonFX(SpindexerConstants.motorID);
+        spindexerMotor = new TalonFX(SpindexerConstants.motorID);
 
         // Configure motors
-        motor.getConfigurator().apply(SpindexerConstants.config);
+        spindexerMotor.getConfigurator().apply(SpindexerConstants.config);
     }
     
     public Command setDutyCycle(double dutyCycle) {
@@ -27,6 +28,6 @@ public class Spindexer extends SubsystemBase{
 
     @Override
     public void periodic() {
-        motor.setControl(request);
+        spindexerMotor.setControl(request);
     }
 }
