595ac75ab3a17d9a0f5190abfd3dc702792d6d9c
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-04-01 17:34:11 -0700

    create a limelight variables string array for visionSubsystem

--- src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/Vision/VisionConstants.java
@@ -1,6 +1,13 @@
 package frc.robot.subsystems.Vision;
 
 public class VisionConstants {
+    public static final String[] limelightNames = {
+        "limelight-frontri",
+        "limelight-frontle",
+        "limelight-backri",
+        "limelight-backle"
+    };
+
     public static final String limelightFrontRightIP = "172.29.0.1";
     public static final String limelightFrontLeftIP = "172.29.1.1";
     public static final String limelightBackRightIP = "172.29.2.1";
