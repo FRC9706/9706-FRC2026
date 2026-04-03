1de4142eb895fdd77353196f54b8641a05cea6ea
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-04-02 21:52:15 -0700

    create floor subsystem

--- src/main/java/frc/robot/subsystems/Floor/FloorSubsystem.java
+++ src/main/java/frc/robot/subsystems/Floor/FloorSubsystem.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.Floor;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import lombok.Getter;
 
@@ -11,28 +10,18 @@ public class FloorSubsystem extends SubsystemBase {
     // Init variables
     @Getter private final TalonFX floorMotor;
 
-    private DutyCycleOut mRequest = new DutyCycleOut(0);
+    @Getter private DutyCycleOut floorRequest = new DutyCycleOut(0);
 
     public FloorSubsystem() {
         floorMotor = new TalonFX(FloorConstants.floorMotorID);
     }
 
     public void setFloorDutyCycle(double dutyCycle) {
-        mRequest = new DutyCycleOut(dutyCycle);
-    }
-
-    public Command toggleFloorOuttake() {
-        return this.runOnce(() -> {
-            if (mRequest.Output != 1) {
-                setFloorDutyCycle(0.20);
-            } else {
-                setFloorDutyCycle(0);
-            }
-        });
+        floorRequest = new DutyCycleOut(dutyCycle);
     }
     
     @Override
     public void periodic() {
-        floorMotor.setControl(mRequest);
+        floorMotor.setControl(floorRequest);
     }
 }
