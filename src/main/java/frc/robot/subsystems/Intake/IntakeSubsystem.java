fb7ab57d3efc7e8d18061ba19f5e38ab39a3b97a
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-04-02 21:54:13 -0700

    rename variables & optimize

--- src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -4,8 +4,6 @@ import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import lib.Networking.DynamicInputs;
 import lombok.Getter;
@@ -16,8 +14,8 @@ public class IntakeSubsystem extends SubsystemBase {
     @Getter private TalonFX rollerMotor;
     @Getter private TalonFX[] extMotors;
 
-    private MotionMagicVoltage extRequest = new MotionMagicVoltage(0);
-    private DutyCycleOut rollerRequest = new DutyCycleOut(0);
+    @Getter private MotionMagicVoltage extRequest = new MotionMagicVoltage(0);
+    @Getter private DutyCycleOut rollerRequest = new DutyCycleOut(0);
 
     public IntakeSubsystem() {
         rollerMotor = new TalonFX(IntakeConstants.rollerMotorID);
@@ -31,9 +29,8 @@ public class IntakeSubsystem extends SubsystemBase {
         extMotors[0].getConfigurator().apply(IntakeConstants.extConfig);
         extMotors[1].getConfigurator().apply(IntakeConstants.extConfig2);
 
-        for (TalonFX motor : extMotors) {
-            motor.setPosition(0);
-        }
+        extMotors[0].setPosition(0);
+        extMotors[1].setPosition(0);
 
         // auto dynamic pid tuning
         DynamicInputs.autoTalonFXMotionMagicPID(
@@ -48,6 +45,7 @@ public class IntakeSubsystem extends SubsystemBase {
             extMotors[1]
         );
     }
+
     public void setExtPos(double pos) {
         extRequest = new MotionMagicVoltage(pos);
     }
@@ -56,42 +54,10 @@ public class IntakeSubsystem extends SubsystemBase {
         rollerRequest = new DutyCycleOut(dutyCycle);
     }
 
-    public Command toggleRollers() {
-        return this.runOnce(() -> {
-            if (rollerRequest.Output != 1) {
-                setRollerDutyCycle(1);
-            } else {
-                setRollerDutyCycle(-1);
-            }
-        });
-    }
-
-    public Command extend() {
-        return this.runOnce(() -> {
-            setExtPos(IntakeConstants.extendedPos);
-        });
-    }
-
-    public Command retract() {
-        return this.runOnce(() -> {
-            setExtPos(IntakeConstants.retractedPos);
-        });
-    }
-    
-    public Command wiggle() {
-        return Commands.repeatingSequence(
-            Commands.runOnce(() -> setExtPos(IntakeConstants.wiggleRange[0])),
-            Commands.waitSeconds(0.5),
-            Commands.runOnce(() -> setExtPos(IntakeConstants.wiggleRange[1])),
-            Commands.waitSeconds(0.5)
-        );
-    }
-
     @Override
     public void periodic() {
-        for (TalonFX motor : extMotors) {
-            motor.setControl(extRequest);
-        }
+        extMotors[0].setControl(extRequest);
+        extMotors[1].setControl(extRequest);
 
         rollerMotor.setControl(rollerRequest);
     }
