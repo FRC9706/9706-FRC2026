a187d7526fe07fc4d992838970d80212f47f0721
nav-util <202357983+Nav-util@users.noreply.github.com>
2026-04-08 18:05:13 -0700

    uncomment

--- src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsystem.java
@@ -1,99 +1,99 @@
-// package frc.robot.subsystems.Intake;
+package frc.robot.subsystems.Intake;
 
-// import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.Logger;
 
-// import com.ctre.phoenix6.controls.DutyCycleOut;
-// import com.ctre.phoenix6.controls.MotionMagicVoltage;
-// import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
 
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import lib.Networking.DynamicInputs;
-// import lombok.Getter;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lib.Networking.DynamicInputs;
+import lombok.Getter;
 
-// public class IntakeSubsystem extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase {
 
-//     // init motor variables
-//     @Getter private TalonFX rollerMotor;
-//     @Getter private TalonFX[] extMotors;
+    // init motor variables
+    @Getter private TalonFX rollerMotor;
+    @Getter private TalonFX[] extMotors;
 
-//     @Getter private MotionMagicVoltage extRequest = new MotionMagicVoltage(0);
-//     @Getter private DutyCycleOut rollerRequest = new DutyCycleOut(0);
+    @Getter private MotionMagicVoltage extRequest = new MotionMagicVoltage(0);
+    @Getter private DutyCycleOut rollerRequest = new DutyCycleOut(0);
 
-//     public IntakeSubsystem() {
-//         rollerMotor = new TalonFX(IntakeConstants.rollerMotorID);
-//         rollerMotor.getConfigurator().apply(IntakeConstants.intakeConfig);
+    public IntakeSubsystem() {
+        rollerMotor = new TalonFX(IntakeConstants.rollerMotorID);
+        rollerMotor.getConfigurator().apply(IntakeConstants.intakeConfig);
         
-//         extMotors = new TalonFX[] {
-//             new TalonFX(IntakeConstants.extMotorIDs[0]),
-//             new TalonFX(IntakeConstants.extMotorIDs[1])
-//         };
+        extMotors = new TalonFX[] {
+            new TalonFX(IntakeConstants.extMotorIDs[0]),
+            new TalonFX(IntakeConstants.extMotorIDs[1])
+        };
 
-//         extMotors[0].getConfigurator().apply(IntakeConstants.extConfig);
-//         extMotors[1].getConfigurator().apply(IntakeConstants.extConfig2);
+        extMotors[0].getConfigurator().apply(IntakeConstants.extConfig);
+        extMotors[1].getConfigurator().apply(IntakeConstants.extConfig2);
 
-//         extMotors[0].setPosition(0);
-//         extMotors[1].setPosition(0);
+        extMotors[0].setPosition(0);
+        extMotors[1].setPosition(0);
 
-//         // auto dynamic pid tuning
-//         DynamicInputs.autoTalonFXMotionMagicPID(
-//             "Intake/Ext Motors[0]", 
-//             IntakeConstants.extConfig, 
-//             extMotors[0]
-//         );
+        // auto dynamic pid tuning
+        DynamicInputs.autoTalonFXMotionMagicPID(
+            "Intake/Ext Motors[0]", 
+            IntakeConstants.extConfig, 
+            extMotors[0]
+        );
 
-//         DynamicInputs.autoTalonFXMotionMagicPID(
-//             "Intake/Ext Motors[1]", 
-//             IntakeConstants.extConfig2, 
-//             extMotors[1]
-//         );
+        DynamicInputs.autoTalonFXMotionMagicPID(
+            "Intake/Ext Motors[1]", 
+            IntakeConstants.extConfig2, 
+            extMotors[1]
+        );
 
 
-//     }
+    }
 
-//     public double getExtMotorsPos(int index) {
-//         return extMotors[index].getPosition().getValueAsDouble();
-//     }
+    public double getExtMotorsPos(int index) {
+        return extMotors[index].getPosition().getValueAsDouble();
+    }
 
-//     public void setExtPos(double pos) {
-//         extRequest = new MotionMagicVoltage(pos);
-//     }
+    public void setExtPos(double pos) {
+        extRequest = new MotionMagicVoltage(pos);
+    }
 
-//     public boolean isExtPosReached() {
-//         if (
-//             (Math.abs(getExtMotorsPos(0) - extRequest.Position)) <= IntakeConstants.extPosTolerance
-//             &&
-//             (Math.abs(getExtMotorsPos(1) - extRequest.Position)) <= IntakeConstants.extPosTolerance
-//         ) {
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
+    public boolean isExtPosReached() {
+        if (
+            (Math.abs(getExtMotorsPos(0) - extRequest.Position)) <= IntakeConstants.extPosTolerance
+            &&
+            (Math.abs(getExtMotorsPos(1) - extRequest.Position)) <= IntakeConstants.extPosTolerance
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-//     public void setRollerDutyCycle(double dutyCycle) {
-//         rollerRequest = new DutyCycleOut(dutyCycle);
-//     }
+    public void setRollerDutyCycle(double dutyCycle) {
+        rollerRequest = new DutyCycleOut(dutyCycle);
+    }
 
-//     @Override
-//     public void periodic() {
-//         extMotors[0].setControl(extRequest);
-//         extMotors[1].setControl(extRequest);
+    @Override
+    public void periodic() {
+        extMotors[0].setControl(extRequest);
+        extMotors[1].setControl(extRequest);
 
-//         rollerMotor.setControl(rollerRequest);
+        rollerMotor.setControl(rollerRequest);
 
-//         Logger.recordOutput("Intake/Extendor/Ext Request", extRequest.Position);
+        Logger.recordOutput("Intake/Extendor/Ext Request", extRequest.Position);
 
-//         Logger.recordOutput("Intake/Extendor/Ext Positions", 
-//             new double[] {getExtMotorsPos(0), getExtMotorsPos(1)}
-//         );
+        Logger.recordOutput("Intake/Extendor/Ext Positions", 
+            new double[] {getExtMotorsPos(0), getExtMotorsPos(1)}
+        );
 
-//         Logger.recordOutput("Intake/Extendor/Ext differences", 
-//             new double[] {
-//                 Math.abs(getExtMotorsPos(0) - extRequest.Position),
-//                 Math.abs(getExtMotorsPos(1) - extRequest.Position)
-//             }
-//         );
+        Logger.recordOutput("Intake/Extendor/Ext differences", 
+            new double[] {
+                Math.abs(getExtMotorsPos(0) - extRequest.Position),
+                Math.abs(getExtMotorsPos(1) - extRequest.Position)
+            }
+        );
 
-//         Logger.recordOutput("Intake/Extendor/Is Ext Pos reached", isExtPosReached());
-//     }
-// }
+        Logger.recordOutput("Intake/Extendor/Is Ext Pos reached", isExtPosReached());
+    }
+}
